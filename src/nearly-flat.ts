abd37e2575ce9060b9da3110a0cee54d38580f9f
Jeremy J Starcher <jeremy.starcher@gmail.com>
2017-11-04 22:14:00 -0400

    White space handled in expressions

---
+++ src/nearly-flat.ts
@@ -0,0 +1,33 @@
+import * as moo from 'moo'
+/*
+Why is parser.results an array? 
+Sometimes, a grammar can parse a particular string in multiple different ways.
+For example, the following grammar parses the string "xyz" in two different ways.
+
+x -> "xy" "z"
+   | "x" "yz"
+
+Such grammars are ambiguous. nearley provides you with all the parsings.
+In most cases, however, your grammars should not be ambiguous
+(parsing ambiguous grammars is inefficient!).
+Thus, the most common usage is to simply query parser.results[0].
+
+You might like to check first that parser.results.length is exactly 1;
+ if there is more than one result, then your grammar is ambiguous!
+*/
+
+export function flatten(tokens: moo.Token[]) {
+    const ret = tokens.map(token => {
+        if (Array.isArray(token)) {
+            const a = token as moo.Token[];
+            if (a.length > 1) {
+                throw new Error("Grammar issue.  Ambiguous language error");
+            } else {
+                return a[0];
+            }
+        } else {
+            return token;
+        }
+    });
+    return ret;
+}
