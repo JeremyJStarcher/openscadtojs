40f97d4d7c33217d49e09dfcfb40f9bbdf1ed5d2
Jeremy J Starcher <jeremy.starcher@gmail.com>
2017-12-16 09:16:56 -0500

    Tokenizer cleanup

--- src/compiler/tokenizer/tokenizer.ts
+++ src/compiler/tokenizer/tokenizer.ts
@@ -7,8 +7,16 @@ export const OPENSCAD_RULES /*:  moo.Rules */ = {
         match: /[a-zA-Z_][0-9a-zA-Z_]*/, keywords: {
             constant_boolean: ['true', 'false'],
             constant_undefined: ['undef'],
-            ifKeyword: ['if', 'else'],
+            ifKeyword: ['if'],
+            elseKeyword: ['else'],
             moduleKeyword: ['module'],
+            functionKeyword: ['function'],
+
+            incudeKeyword: ['include'],
+            useKeyword: ['use'],
+            forKeyword: ['for'],
+            intersectionForKeyword: ['intersection_for'],
+            assignKeyword: ['assign'],
         }
     },
     special_identifier: /[\$]{1}[_a-zA-Z][0-9a-zA-Z_]*/,
@@ -16,7 +24,7 @@ export const OPENSCAD_RULES /*:  moo.Rules */ = {
     lparen: '(',
     rparen: ')',
     eos: ';',
-    keyword: ['function', 'include', 'use', 'for', 'intersection_for', 'assign'],
+    keyword: [],
     lbrace: '{',
     rbrace: '}',
     lbracket: '[',
